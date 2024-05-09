062960de787243e914d5f1a29e6f88252112af83
kajtek <kajtekkajtek2003@gmail.com>
2024-05-09 18:07:16 +0200

    sinusoida zaimplementowana

--- projekt_mmm.cpp
+++ projekt_mmm.cpp
@@ -1,21 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <cmath>
+#define Pi 3.141592
 using namespace std;
 
 int main(){
 	//setup**************************************
 ifstream wejscie("dane.txt");
 ofstream wyjscie("wyjscie.txt");
-double m1,m2,k1,k2,b1,b2,u=1,X1,X2,dT=0.1,V1,V2,A1,A2;
-wejscie>>m1>>m2>>k1>>k2>>b1>>b2;
+double m1,m2,k1,k2,b1,b2,X1,u,X2,dT=0.1,V1,V2,A1,A2,a,T;
+bool czy_sinusoida;
+wejscie>>czy_sinusoida;
+if(czy_sinusoida){
+	wejscie>>T>>a>>m1>>m2>>k1>>k2>>b1>>b2;
+}else{
+	wejscie>>a>>m1>>m2>>k1>>k2>>b1>>b2;
+}
+
 V1=0;
 V2=0;
 A1=0;
 A2=0;
 X1=0;
 X2=0;
+u=a;
 	//matma**************************************
-for(int i=0;i<600;i++){
+	
+for(int i=0;i<1000;i++){
+	if(czy_sinusoida){
+		u=a*sin((1/T)*2*PI*dT*i)+0.5*a;
+	}
 	A2=u/m2-(X2-X1)*k2/m2-(V2-V1)*b2/m2;
 	A1=-k1*X1/m1+k2*(X2-X1)/m1-b1*V1/m1+b2*(V2-V1)/m1;
 	V2=V2+A2*dT;
@@ -25,9 +39,6 @@ for(int i=0;i<600;i++){
 	wyjscie<<i*dT<<";";
 	wyjscie<<X1<<";";
 	wyjscie<<X2<<endl;
-	if(i==100){
-		u=0;
-	}
 }
 
 
